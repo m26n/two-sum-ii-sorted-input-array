d77cfd61d3bec75ff9264416307b348343f5339f
Maurice Bergerhausen <mbn@fastmail.de>
2024-04-16 21:31:31 +0200

    second try

--- main.py
+++ main.py
@@ -9,20 +9,44 @@ class Solution:
         while left < right:
             if numbers[left] + numbers[right] == target:
                 return [left + 1, right + 1]
-            if numbers[left] + numbers[right] > target:
+            if numbers[left] + numbers[right-1] >= target:
                 right -= 1
                 continue
-            if numbers[left] + numbers[right] > target:
+            if numbers[left+1] + numbers[right] >= target:
                 left += 1
                 continue
 
 
-numbers = [2, 7, 11, 15]
-target = 9
-output = [1, 2]
 sol = Solution()
-twoSums = sol.twoSum(numbers, target)
 
-assert twoSums == output
+numbers1 = [2, 7, 11, 15]
+target1 = 9
+output1 = [1, 2]
 
+twoSums = sol.twoSum(numbers1, target1)
 
+assert twoSums == output1
+
+numbers2 = [2, 3, 4]
+target2 = 6
+output2 = [1, 3]
+
+twoSums = sol.twoSum(numbers2, target2)
+
+assert twoSums == output2
+
+numbers3 = [-1, 0]
+target3 = -1
+output3 = [1, 2]
+
+twoSums = sol.twoSum(numbers3, target3)
+
+assert twoSums == output3
+
+numbers4 = [5, 25, 75]
+target4 = 100
+output4 = [2, 3]
+
+twoSums = sol.twoSum(numbers4, target4)
+
+assert twoSums == output4
